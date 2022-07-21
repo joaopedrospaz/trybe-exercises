9e361fc31865f7980e11682f4093a0a57e3a8e3f
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-07-20 22:53:02 -0300

    Adiciona exercicio para fixar

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,54 @@
+// Parte de Variáveis
+const myName = "João Pedro";
+const birthCity = "Cruz das Almas";
+let birthYear = 1999;
+
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//  Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+patientId = '50';
+console.log(typeof patientId);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+
+// Condições If e Else
+const nota = 90;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)")
+}
+
+const conditionOne = true;
+const conditionTwo = false;
+
+console.log(conditionOne && conditionTwo);
+
+
+
