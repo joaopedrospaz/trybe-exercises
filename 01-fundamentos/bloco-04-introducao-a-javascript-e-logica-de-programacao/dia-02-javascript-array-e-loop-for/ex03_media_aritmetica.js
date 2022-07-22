6ff4d94ebcbda043773a5ed18343111bc15cdd81
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-07-22 00:36:47 -0300

    Adiciona parte 6 - quantidade de ímpares

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex03_media_aritmetica.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex03_media_aritmetica.js
@@ -3,9 +3,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
 for (index = 0; index < numbers.length; index += 1) {
-    sum += numbers[index]
+    sum += numbers[index];
 }
 
-let average = sum / numbers.length
+let average = sum / numbers.length;
 
 console.log(average);
