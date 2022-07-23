cc7fe58eaa9c0af0a61156b9b73f6a96938d7194
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-07-22 23:22:30 -0300

    Adiciona parte 1 e 2 do bônus

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exbonus.js
@@ -0,0 +1,24 @@
+// parte 1 - quadrado
+let n = 5;
+let line1 = ''
+
+for (let index = 0; index < n; index += 1) {
+    line1 += '*'    
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(line1);
+}
+
+// parte 2 - triângulo 
+let line2 = '';
+for (let index = 0; index < n; index += 1) {
+    line2 += '*'  
+    console.log(line2);  
+}
+
+// parte 2 - triângulo invertido
+let line3 = '';
+for (let index = 0; index < n; index += 1) {
+    line2 += '*'  
+    console.log(line3);  
