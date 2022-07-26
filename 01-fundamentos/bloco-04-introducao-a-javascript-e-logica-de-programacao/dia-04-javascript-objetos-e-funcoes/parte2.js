7ffd96d54d9d2ed275992f69afac42fe13f0e942
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-07-26 09:36:14 -0300

    Adiciona parte 2 completa

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/parte2.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/parte2.js
@@ -1,4 +1,4 @@
-// parte 1 -  palíndromo 
+// parte 1 -  verifica se a palavra é um palíndromo. 
 function verificaPalindrome(word) {
     if (word === word.split('').reverse().join('')) {
         return true;
@@ -11,7 +11,7 @@ console.log(verificaPalindrome('tenet'));
 console.log('*************************');
 
 
-// parte 2 - indice do maior valor 
+// parte 2 - indice do maior valor. 
 function higherNumber(numbers) {
     let higher = numbers[0];
     let indice = 0;
@@ -27,7 +27,7 @@ function higherNumber(numbers) {
 console.log(higherNumber([2, 3, 6, 7, 10, 1]));
 console.log('*************************');
 
-// parte 3 - indice do menor valor 
+// parte 3 - indice do menor valor.
 function smallestnumber(numbers2) {
     let smaller = numbers2[0];
     let indice = 0;
@@ -43,7 +43,7 @@ function smallestnumber(numbers2) {
 console.log(smallestnumber([2, 4, 6, 7, 10, 0, -3]));
 console.log('*************************');
 
-// parte 4 - nome de mais caracteres 
+// parte 4 - nome de mais caracteres.
 function biggestName(names) {
     let higher = names[0].length;
     let bigger = names[0];
@@ -60,3 +60,50 @@ function biggestName(names) {
 console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 console.log('*************************');
 
+// parte 5 - número que mais se repete.
+function maisRepetido(numeros) {
+    let num = 0;
+    let repeticoes = 0;
+    for (index in numeros) {
+        let contagem = 0;
+        for (indice in numeros) {
+            if (numeros[index] === numeros[indice]) {
+                contagem += 1;
+                if (contagem > repeticoes) {
+                    repeticoes = contagem;
+                    num = numeros[index];
+                }
+            }
+        }
+    }
+    return num;
+}
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
+console.log('*************************');
+
+// parte 6 - soma todos os núemros até n.
+function sum(n) {
+    let total = 0;
+    for (let index = 1; index <= n; index += 1) {
+        total += index;
+    }
+    return total
+}
+console.log(sum(5));
+console.log('*************************');
+
+// parte 7 - verificar se uma palavra é o final da outra.
+function verificaFimPalavra(word1, word2) {
+    let plavra = word1.split('').reverse().join('');
+    let fimDaPalavra = word2.split('').reverse().join('');
+    let resultado = true;
+    for (let index = 0; index < word2.length; index += 1) {
+        if (plavra[index] !== fimDaPalavra[index]) {
+            resultado = false;
+            break
+        }
+    }
+    return resultado;
+}
+console.log(verificaFimPalavra('trybe', 'be'));
+console.log('*************************');
