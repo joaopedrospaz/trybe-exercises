7c603918c3f7d14f81bdc123b1d754da5c25a2d2
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-08-05 12:49:10 -0300

    Adiciona até a parte 3

---
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,12 @@
+function handleSubmit(event) {
+    event.preventDefault();
+}
+window.onload = function () {
+    const submitBtn = document.querySelector('#btn-submit');
+    submitBtn.addEventListener('click', handleSubmit)
+}
+
+
+function clear() {
+    
+}
