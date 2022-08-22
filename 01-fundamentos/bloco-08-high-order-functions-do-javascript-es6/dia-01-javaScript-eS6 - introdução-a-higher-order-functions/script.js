9e43953de0240a8835d81514071440a683e8eb08
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-08-22 13:41:38 -0300

    Finaliza parte 2 do bonus

--- 01-fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-01-javaScript-eS6 - introdução-a-higher-order-functions/script.js
+++ 01-fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-01-javaScript-eS6 - introdução-a-higher-order-functions/script.js
@@ -22,4 +22,55 @@ const check = (number, numberDrawn) => {
 
 const result = () => Math.floor((Math.random() * 5) + 1);
 console.log(check(3, result()));
+
 // parte 3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+point = (listRight, listAnswers) => {
+  if (listRight === listAnswers) {
+    return 1;
+  } else if (listAnswers === 'N.A') {
+    return 0;
+  } return -0.5;
+};
+
+const test = (right, answers, func) => {
+  let total =0;
+  for (let index = 0; index < right.length; index += 1) {
+    total += func(right[index], answers[index]);
+  }
+  return total;
+}
+console.log(test(RIGHT_ANSWERS, STUDENT_ANSWERS, point));  
+
+// bonus
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+dragonMax = dragon.strength;
+const dragonDamage = () => Math.floor((Math.random() * (dragonMax - 15 + 1)) + 15);
+
+warriorMin = warrior.strength;
+warriorMax = warrior.strength * warrior.weaponDmg;
+const warriorDamage = () => Math.floor((Math.random() * (warriorMax - warriorMin + 1)) + warriorMin);
