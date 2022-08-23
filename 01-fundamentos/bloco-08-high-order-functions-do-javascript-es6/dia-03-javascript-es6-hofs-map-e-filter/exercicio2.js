d089f8fd877558feed6e3a955fa86cee96abbf77
João Pedro Silva da Paz <joaopaz79@outlook.com>
2022-08-23 18:46:17 -0300

    Adiciona parte 2

--- 01-fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-03-javascript-es6-hofs-map-e-filter/exercicio2.js
+++ 01-fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-03-javascript-es6-hofs-map-e-filter/exercicio2.js
@@ -62,15 +62,33 @@ const books = [
 ];
 
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
 ];
 
-function formatedBookNames() {
-  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`)
+function nameAndAge() {
+  return books.map((book) => ({age: book.releaseYear - book.author.birthYear, author: book.author.name})).sort((authorA, authorB) => authorA.age - authorB.age);
 }
-// console.log(formatedBookNames());
+console.log(nameAndAge());
